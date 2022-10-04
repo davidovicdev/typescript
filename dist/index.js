a671ceda16f080866e033f8a0b86c3270b9676c1
Matija Davidovic <davidovicdev@gmail.com>
2022-10-04 09:58:54 +0200

    100%

--- dist/index.js
+++ dist/index.js
@@ -13,10 +13,10 @@ Item (id,name,price)
 Food,Book etc (pages , calories, isVegetarian etc)
 Iskoristiti Type,Interface,generic je plus
  */
-var deleteButtons = [];
 var products = [];
 class Product {
-    constructor(name, type, price, description) {
+    constructor(id, name, type, price, description) {
+        this.id = id;
         this.name = name;
         this.type = type;
         this.price = price;
@@ -26,55 +26,58 @@ class Product {
         console.log("RADI DELETE DUGME IZ KLASE");
     }
 }
-var table = document.getElementById("table");
 var createButton = document.getElementById("createButton");
+var counter = 1;
 createButton === null || createButton === void 0 ? void 0 : createButton.addEventListener("click", function (e) {
     e.preventDefault();
     // VALIDATION
     //        DROPDOWN MENU (TYPES)
     var name = document.getElementById("name");
-    if ((name === null || name === void 0 ? void 0 : name.value) == "") {
-        alert("Please enter the product name");
-        return;
-    }
+    /* if (name?.value == "") {
+      alert("Please enter the product name");
+      return;
+    } */
     var price = document.getElementById("price");
     var type = document.getElementById("type");
     var valueId = type.selectedIndex;
     var valueText = type.options[valueId].text;
-    if (valueId == 0) {
-        alert("Please choose the type of product");
-        return;
-    }
+    /* if (valueId == 0) {
+      alert("Please choose the type of product");
+      return;
+    } */
     var description = document.getElementById("description")
         .value;
-    const product = new Product(name.value, valueText, price.value, description);
+    const product = new Product(counter, name.value, valueText, price.value, description);
+    counter++;
     products.push(product);
-    table === null || table === void 0 ? void 0 : table.innerHTML = "";
-    // OUTPUT
+    setTable(products);
+});
+/* DELETE */
+var table = document.getElementById("table");
+function setTable(products) {
+    var counter = 1;
+    table.innerHTML = "";
     products.forEach((product) => {
-        table === null || table === void 0 ? void 0 : table.innerHTML += `<tr>
+        table.innerHTML += `<tr>
+    <td>${product.id}</td>
     <td>${product.name}</td>
     <td>${product.type}</td>
     <td>${product.price}</td>
     <td>${product.description}</td>
-    <td class='delete'></td>
+    <td class='btn btn-danger' onClick='test(${product.id})'>DELETE</td>
     </tr>`;
     });
-});
-var deleteButton = document.getElementById("deleteButton");
-deleteButton === null || deleteButton === void 0 ? void 0 : deleteButton.addEventListener("click", function (e) {
-    e.preventDefault();
-    var deleteButtons = Array.from(document.getElementsByClassName("delete"));
-    deleteButtons.forEach((db) => {
-        db.innerHTML = "<button class='text-danger x'>X</button>";
-    });
-    var xs = Array.from(document.getElementsByClassName("x"));
-    xs.forEach((x) => {
-        x.addEventListener("click", function (e) {
-            var _a;
-            console.log("RADI");
-            e.preventDefault();
-            (_a = x.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement.remove();
-        });
-    });
-});
+    counter++;
+}
+function test(id) {
+    console.log(products);
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id == id) {
+            const index = products.indexOf(products[i], 0);
+            if (index > -1) {
+                products.splice(index, 1);
+            }
+        }
+    }
+    setTable(products);
+}
--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,16 @@ Item (id,name,price)
 Food,Book etc (pages , calories, isVegetarian etc)
 Iskoristiti Type,Interface,generic je plus
  */
-var deleteButtons = [];
 var products: Product[] = [];
 interface IProduct {
+  id: number;
   name: string;
   type: string;
   price: string;
   description?: string;
-  delete(): void;
 }
 class Product implements IProduct {
+  id: number;
   name: string;
   type: string;
   price: string;
@@ -29,7 +29,14 @@ class Product implements IProduct {
   delete(): void {
     console.log("RADI DELETE DUGME IZ KLASE");
   }
-  constructor(name: string, type: string, price: string, description?: string) {
+  constructor(
+    id: number,
+    name: string,
+    type: string,
+    price: string,
+    description?: string
+  ) {
+    this.id = id;
     this.name = name;
     this.type = type;
     this.price = price;
@@ -37,55 +44,66 @@ class Product implements IProduct {
   }
 }
 
-var table = document.getElementById("table") as HTMLTableElement;
 var createButton = document.getElementById("createButton");
+var counter = 1;
 createButton?.addEventListener("click", function (e) {
   e.preventDefault();
   // VALIDATION
   //        DROPDOWN MENU (TYPES)
   var name = document.getElementById("name") as HTMLInputElement;
-
-  if (name?.value == "") {
+  /* if (name?.value == "") {
     alert("Please enter the product name");
     return;
-  }
+  } */
   var price = document.getElementById("price") as HTMLInputElement;
   var type = document.getElementById("type") as HTMLSelectElement;
   var valueId = type.selectedIndex;
   var valueText = type.options[valueId].text;
-  if (valueId == 0) {
+  /* if (valueId == 0) {
     alert("Please choose the type of product");
     return;
-  }
+  } */
   var description = (document.getElementById("description") as HTMLInputElement)
     .value;
-  const product = new Product(name.value, valueText, price.value, description);
+
+  const product = new Product(
+    counter,
+    name.value,
+    valueText,
+    price.value,
+    description
+  );
+  counter++;
   products.push(product);
-  table?.innerHTML = "";
-  // OUTPUT
+  setTable(products);
+});
+
+/* DELETE */
+var table = document.getElementById("table") as HTMLTableElement;
+function setTable(products: Product[]): void {
+  var counter = 1;
+  table.innerHTML = "";
   products.forEach((product) => {
-    table?.innerHTML += `<tr>
+    table.innerHTML += `<tr>
+    <td>${product.id}</td>
     <td>${product.name}</td>
     <td>${product.type}</td>
     <td>${product.price}</td>
     <td>${product.description}</td>
-    <td class='delete'></td>
+    <td class='btn btn-danger' onClick='test(${product.id})'>DELETE</td>
     </tr>`;
   });
-});
-var deleteButton = document.getElementById("deleteButton");
-deleteButton?.addEventListener("click", function (e) {
-  e.preventDefault();
-  var deleteButtons = Array.from(document.getElementsByClassName("delete"));
-  deleteButtons.forEach((db) => {
-    db.innerHTML = "<button class='text-danger x'>X</button>";
-  });
-  var xs = Array.from(document.getElementsByClassName("x"));
-  xs.forEach((x) => {
-    x.addEventListener("click", function (e) {
-      console.log("RADI");
-      e.preventDefault();
-      x.parentElement?.parentElement.remove();
-    });
-  });
-});
+  counter++;
+}
+function test(id: number) {
+  console.log(products);
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id == id) {
+      const index = products.indexOf(products[i], 0);
+      if (index > -1) {
+        products.splice(index, 1);
+      }
+    }
+  }
+  setTable(products);
+}
